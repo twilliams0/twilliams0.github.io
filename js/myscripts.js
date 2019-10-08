8d9e9754492687b81e1bef6f3bfc384e7790fc6a
Tyler <t.williams@ebsco.om>
2019-10-08 10:17:05 -0500

    adding map and styles

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,10 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 0) {
-        $('.fade-break').fadeOut();
-    } else {
-        $('.fade-break').fadeIn();
-    }
+$(window).scroll(function() {
+if ($(this).scrollTop()<300)
+{
+$('.back-to-top').hide(100);
+}
+else
+{
+$('.back-to-top').show(100);
+}
 });
